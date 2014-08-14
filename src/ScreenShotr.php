ea1ed28b94aa6e4f15b7ee0f6dfb573c6e712150
Aaron Scherer <aequasi@gmail.com>
2014-08-14 13:14:46 -0700

    Adding generateScreenshot

--- src/ScreenShotr.php
+++ src/ScreenShotr.php
@@ -9,13 +9,52 @@
  * with this source code in the file LICENSE
  */
 
-namespace Aequasi;
+namespace ScreenShotr;
+
+use ScreenShotr\Exception\NoFrameException;
 
 /**
  * @author Aaron Scherer <aequasi@gmail.com>
  */
-class ScreenShotr
+class Core
 {
+    /**
+     * @var string $screenshotDirectory
+     */
+    public static $screenshotDirectory = '/tmp';
 
+    /**
+     * @param $file
+     * @param $frameNumber
+     *
+     * @return string
+     * @throws Exception\NoFrameException
+     * @throws \Exception
+     */
+    public static function generateScreenshot($file, $frameNumber)
+    {
+        if (!file_exists($file)) {
+            throw new \Exception("The file '{$file}' does not exist.'");
+        }
+
+        $movie = new \ffmpeg_movie($file, false);
+        $frame = $movie->getFrame($frameNumber);
+
+        if ($frame === false) {
+            throw new NoFrameException("There is no frame located at {$frameNumber} in the movie: {$file}.");
+        }
+
+        $gd_image = $frame->toGDImage();
+        $file = tempnam(static::$screenshotDirectory, 'ss_');
+        if ($file === false) {
+            throw new \Exception("Could not create a file in: ".static::$screenshotDirectory);
+        }
+
+
+        imagepng($gd_image, $file);
+        imagedestroy($gd_image);
+
+        return $file;
+    }
 }
 
