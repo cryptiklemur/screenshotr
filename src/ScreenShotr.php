ae23d6cd5edd08426b14d37a0bdd7e32f1664a44
Aaron Scherer <aequasi@gmail.com>
2014-08-14 14:04:27 -0700

    Adding new methods, retuning SplFileInfo instead

--- src/ScreenShotr.php
+++ src/ScreenShotr.php
@@ -23,6 +23,55 @@ class Core
      */
     public static $screenshotDirectory = '/tmp';
 
+    public static function getMovie($file)
+    {
+        if (!file_exists($file)) {
+            throw new \Exception("The file '{$file}' does not exist.'");
+        }
+
+        return new \ffmpeg_movie($file, false);
+    }
+
+    /**
+     * @param \ffmpeg_movie $movie
+     * @param               $frameNumber
+     *
+     * @return \ffmpeg_frame
+     * @throws Exception\NoFrameException
+     */
+    public static function getFrame(\ffmpeg_movie $movie, $frameNumber)
+    {
+        $frame = $movie->getFrame($frameNumber);
+
+        if ($frame === false) {
+            throw new NoFrameException(
+                "There is no frame located at {$frameNumber} in the movie: {$movie->getFilename()}."
+            );
+        }
+
+        return $frame;
+    }
+
+    /**
+     * @param \ffmpeg_frame $frame
+     *
+     * @return \SplFileInfo
+     * @throws \Exception
+     */
+    public static function getScreenshot(\ffmpeg_frame $frame)
+    {
+        $gd_image = $frame->toGDImage();
+        $file     = tempnam(static::$screenshotDirectory, 'ss_');
+        if ($file === false) {
+            throw new \Exception("Could not create a file in: ".static::$screenshotDirectory);
+        }
+
+        imagepng($gd_image, $file);
+        imagedestroy($gd_image);
+
+        return new \SplFileInfo($file);
+    }
+
     /**
      * @param $file
      * @param $frameNumber
@@ -33,28 +82,20 @@ class Core
      */
     public static function generateScreenshot($file, $frameNumber)
     {
-        if (!file_exists($file)) {
-            throw new \Exception("The file '{$file}' does not exist.'");
+        return static::getScreenshot(static::getFrame(static::getMovie($file), $frameNumber));
+    }
+
+    public static function generateScreenshotEveryFrame($file, $frameCount = 1)
+    {
+        $movie       = static::getMovie($file);
+        $screenShots = [];
+        $i           = 1;
+        while ($frame = static::getFrame($movie, $i)) {
+            $screenShots[$i] = static::getScreenshot($frame);
+            $i += $frameCount;
         }
 
-        $movie = new \ffmpeg_movie($file, false);
-        $frame = $movie->getFrame($frameNumber);
-
-        if ($frame === false) {
-            throw new NoFrameException("There is no frame located at {$frameNumber} in the movie: {$file}.");
-        }
-
-        $gd_image = $frame->toGDImage();
-        $file = tempnam(static::$screenshotDirectory, 'ss_');
-        if ($file === false) {
-            throw new \Exception("Could not create a file in: ".static::$screenshotDirectory);
-        }
-
-
-        imagepng($gd_image, $file);
-        imagedestroy($gd_image);
-
-        return $file;
+        return $screenShots;
     }
 }
 
